08680037ca3feb919cf70e13e83f889fa3d02d19
Zudog <Zudog@Zudogui-MacBook-Pro.local>
2016-05-13 23:34:26 +0900

    TT

--- api/views.py
+++ api/views.py
@@ -81,7 +81,7 @@ def getAnalytics(request):
     # followings = api.user_followed_by(user_id)
 
     # print followings
-    return HttpResponse(mediacaptions)
+    return HttpResponse(result)
 
 def getToken(request):
     api = InstagramAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri="http://bestjae.com/api/getToken")
